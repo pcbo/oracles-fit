aeab1e58ab8d58c3dd3ca7b8efa7e617e42c1cdb
Francisco Leal <francisco@talentprotocol.com>
2024-06-30 17:07:39 +0100

    fix: mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex flex-col h-48 w-full items-center justify-center gap-2 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        <div className="flex flex-col h-48 w-full items-center justify-center gap-2 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <Image
             src="/oracles-logo.png"
             alt="Get Fit Logo"
